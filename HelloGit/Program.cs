eef3ae37abb1b9bccf7d4ba5dc9377a674346341
univem <univem@univem.edu.br>
2017-02-20 20:59:52 -0300

    primeiro commit

---
+++ HelloGit/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloGit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Git");
+            Console.ReadKey();
+
+        }
+    }
+}
